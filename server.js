8f4dc4a6d8eba51c2b98f7865e95671ccad17800
osalg22 <osalg22@gmail.com>
2017-05-14 18:28:16 +0000

    get city working

--- server.js
+++ server.js
@@ -1,10 +1,61 @@
 var express = require('express');
+var path = require("path")
 var app = express();
 
+function fixMonth(num) {
+  switch (num) {
+    case 1:
+      return 'January';
+    case 2:
+      return 'February';
+    case 3:
+      return 'March';
+    case 4:
+      return 'April';
+    case 5:
+      return 'May';
+    case 6:
+      return 'June';
+    case 7:
+      return 'July';
+    case 8:
+      return 'August';
+    case 9:
+      return 'September';
+    case 10:
+      return 'October';
+    case 11:
+      return 'November';
+    case 12:
+      return 'December';
+  }
+}
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:date', function (req, res) {
+  var input = req.params.date;
+  if (!input.includes(' ')) input = Number(input)*1000;
+  var date = new Date(input);
+  var unix, natural;
+  
+  if (!date.getTime()) {
+    unix = null;
+    natural = null;
+  } else {
+    unix = Math.floor(date.getTime() / 1000);
+    natural = fixMonth(date.getMonth()+1) + " " + date.getDate() + ", " + date.getFullYear(); 
+  }
+  
+  res.send({
+    "unix": unix,
+    "natural": natural
+  });
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port 8080!');
+  //console.log(new Date("/December, 15 2015").getMonth());
 })
